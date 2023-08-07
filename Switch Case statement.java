b11ac7b17af00b89be16f689763d3fe1527db777
SANJITH KUMAR M <108807863+sanjith1309@users.noreply.github.com>
2023-08-07 21:52:54 +0530

    Create Switch Case statement.java

---
+++ Switch Case statement.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static double areaSwitchCase(int ch, double []a) {
+        double num =0.0;
+        switch(ch){
+            case 1:
+            num = Math.PI*a[0]*a[0];
+            break;
+            case 2:
+            num = a[0]*a[1];
+        }
+        num = Double.parseDouble(String.format("%.5f", num));
+        return num;
+    }
+}
